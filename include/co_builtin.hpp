14e7616aabfb1974af378db6537241f13544ca6b
Dmitriy A <d.arbitman@gmail.com>
2021-07-24 16:34:00 -0700

    Cleanup

--- include/co_builtin.hpp
+++ include/co_builtin.hpp
@@ -17,12 +17,18 @@ struct frame_base {
 };
 
 /// address points to frame_base which will be used to call the appropriate resume, destroy and done functions
-void __builtin_coro_resume(void* address);
-void __builtin_coro_destroy(void* address);
-bool __builtin_coro_done(void* address);
+inline void __builtin_coro_resume(void* address) { static_cast<frame_base*>(address)->resumeFN(address); }
+inline void __builtin_coro_destroy(void* address) { static_cast<frame_base*>(address)->destroyFN(address); }
+inline bool __builtin_coro_done(void* address) { return static_cast<frame_base*>(address)->doneFN(address); }
 
 /// If from_promise is true, then requires an address to the promise that's within a frame and returns the address to
 /// the frame. If from_promise is false, then takes an address to a
-void* __builtin_coro_promise(void* address, int alignment, bool from_promise = true);
+inline void* __builtin_coro_promise(void* address, int alignment, bool from_promise = true) {
+    if (from_promise) {
+        return static_cast<char*>(address) - sizeof(frame_base);
+    } else {
+        return static_cast<char*>(address) + sizeof(frame_base);
+    }
+}
 
 }  // namespace co
